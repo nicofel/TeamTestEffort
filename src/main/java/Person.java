f88cd6839a96b13d76e3c6f3d032594461101a42
unknown <frridaa_johansson@hotmail.com>
2020-11-27 10:48:15 +0100

    Ny class Person

---
+++ src/main/java/Person.java
@@ -0,0 +1,27 @@
+public class Person {
+
+        private String name;
+        private String age;
+        private String telephoneNumber;
+
+        public Person(String name, String age, String telephoneNumber) {
+            this.name = name;
+            this.age = age;
+            this.telephoneNumber = telephoneNumber;
+        }
+
+        public String getName() {
+            return name;
+        }
+        public String getAge() {
+            return age;
+        }
+        public String getTelephoneNumber() {
+            return telephoneNumber;
+        }
+
+    }
+
+
+
+
