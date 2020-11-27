08a974f3c9fe64653cf53fe642fecd3031a6047c
Sona Rahimova <suay_999@hotmail.com>
2020-11-27 10:38:41 +0100

    class contact

---
+++ src/main/java/Contact.java
@@ -0,0 +1,14 @@
+public class Contact {
+    public Contact(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+}
